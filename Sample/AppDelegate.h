01fad1222d512c6598acc5ed7e50479b735c92c2
Poonam <poonam@Poonams-Mac-mini.local>
2016-05-21 15:31:29 +0530

    Product files added

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Poonam on 21/05/16.
+//  Copyright © 2016 Poonam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Poonam on 21/05/16.
+//  Copyright © 2016 Poonam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
